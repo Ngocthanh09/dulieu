404e6241f5a7203451cb9419ab37f8a2877a21d0
Ngocthanh09 <33902461+Ngocthanh09@users.noreply.github.com>
2017-12-04 21:11:12 +0700

    Update bannerdautrang.js

--- bannerdautrang.js
+++ bannerdautrang.js
@@ -117,7 +117,7 @@ $(window).load(function() {
 			$(divpopup).attr('id', 'myModal');
 			$(divpopup).attr('class', 'linhnguyen-modal');
 			$( "body" ).append(divpopup);
-			$("#myModal").html("<a href='http://bit.ly/2iKDYSg' target='_blank'><img src='https://4.bp.blogspot.com/-tY7XrkFs3xw/WiVOC6WKsBI/AAAAAAAACtI/5-toJp0vdi4KRdw3Ige65f-5f4acHdc5ACLcBGAs/s320/BANNER%2BGDB.jpg' width='500px'/><br/>Dự án golden bay</a>. Liên hệ: 0932 14 66 61 -Mr Thành. <h2><a class='close-linhnguyen-modal'>X</a></h2>");
+			$("#myModal").html("<a href='http://bit.ly/2iKDYSg' target='_blank'><img src='https://4.bp.blogspot.com/-vha1wylv_F4/WiVXTNOhgoI/AAAAAAAACtY/rNKHZScequAUoRaJkTGeCL0vkhdX7iLIQCLcBGAs/s640/Quang-truong.jpg' width='500px'/><br/>Dự án golden bay</a>. Liên hệ: 0932 14 66 61 -Mr Thành. <h2><a class='close-linhnguyen-modal'>X</a></h2>");
 			$('#myModal').linhnguyen($('#myModal').data());
 		}
 });
